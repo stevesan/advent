af74a38ed2969a41f9683aff5c6f16319fd50e0e
Steven An <1388372+stevesan@users.noreply.github.com>
2022-12-17 12:42:59 -0500

    Update d16a.py
    greatly simplified to just a stack of states; also realized we're counting pressure released, not total rate

--- d16a.py
+++ d16a.py
@@ -21,80 +21,90 @@ hmm nah, it's not necessarily true that you need to open 15 valves for max relea
 class SearchState:
   opened: list[Node]
   path: list[Node]
-  def curr_node(self): return self.path[-1]
+  pressure_released:int = 0
+
+  def curr_node(self):
+    return self.path[-1]
+
   def get_time(self):
     return len(self.path) - 1 + len(self.opened)
-  def get_score(self):
-    return sum([node.rate for node in self.opened])
-  def clone(self):
-    return SearchState(opened=list(self.opened), path=list(self.path))
 
-@dataclass
-class SearchAction:
-  start_state: SearchState
-  type: str # 'move' to dest or 'open' current valve
-  move_dest: Node = None
+  def get_total_rate(self):
+    return sum([node.rate for node in self.opened])
+
+  def clone(self):
+    return SearchState(opened=list(self.opened), path=list(self.path), pressure_released=self.pressure_released)
+
+  def release_pressure_for_one_minute(self):
+    self.pressure_released += self.get_total_rate()
 
 def find_max_release(name2node:dict[str, Node]):
   init_node = name2node['AA']
   init_state = SearchState(opened=[], path=[init_node])
-  action_stack:list[SearchAction] = []
+  assert init_state.get_time() == 0
+  states_to_explore:list[SearchState] = [init_state]
 
-  def push_actions_from(state:SearchState):
-    node = state.path[-1]
-    if not state.curr_node() in state.opened:
-      open_action = SearchAction(start_state=state, type='open')
-      action_stack.append(open_action)
-    for nbor in node.nbors:
-      # Note we need to allow moving to previously visited nodes. For the dead-end case.
-      move_action = SearchAction(start_state=state, type='move', move_dest=nbor)
-      action_stack.append(move_action)
-
-  push_actions_from(init_state)
   best_score = None
-  while action_stack:
-    # Apply the action to get the new state. If we're out of time, update best_score. If not, generate further actions.
-    action = action_stack.pop(-1)
-    new_state = action.start_state.clone()
-    if action.type == 'open':
-      new_state.opened.append(new_state.curr_node())
-    else:
-      assert action.type == 'move' and action.move_dest
-      new_state.path.append(action.move_dest)
+  while states_to_explore:
+    state:SearchState = states_to_explore.pop(-1)
+    # print('-----')
+    # print([node.name for node in state.path])
+    # print([node.name for node in state.opened])
+    # print(best_score)
+    if state.get_time() >= 30:
+      # Can't explore further.
+      if best_score is None or state.pressure_released > best_score:
+        best_score = state.pressure_released
+        print(f'improved to {best_score}')
+      continue
 
-    if new_state.get_time() >= 30:
-      if best_score is None or new_state.get_score() > best_score:
-        best_score = new_state.get_score()
-    else:
-      push_actions_from(new_state)
+    # Keep exploring from this state
+    if not state.curr_node() in state.opened:
+      # We could open this valve
+      opened_state = state.clone()
+      opened_state.opened.append(state.curr_node())
+      opened_state.release_pressure_for_one_minute()
+      states_to_explore.append(opened_state)
+
+    # We could move to any nbor
+    # Note we need to allow moving to previously visited nodes. For the dead-end case.
+    for nbor in state.curr_node().nbors:
+      moved_state = state.clone()
+      moved_state.path.append(nbor)
+      moved_state.release_pressure_for_one_minute()
+      states_to_explore.append(moved_state)
 
   print(best_score)
-name2node:dict[str, Node] = {}
 
-with open(sys.argv[1]) as f:
-  for line in f:
-    line = line.strip()
-    """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"""
-    # print(line)
-    parts = line.split(' ')
-    assert parts[0] == 'Valve'
-    name = parts[1]
 
-    ratestr = parts[4]
-    assert ratestr[-1] == ';'
-    rate = int(ratestr[len('rate='):-1])
-    
-    assert parts[8] in ['valves', 'valve']
-    tunnelstrs = parts[9:]
-    nbor_names = [s.replace(',', '') for s in tunnelstrs]
+def main():
+  name2node:dict[str, Node] = {}
+  with open(sys.argv[1]) as f:
+    for line in f:
+      line = line.strip()
+      """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"""
+      # print(line)
+      parts = line.split(' ')
+      assert parts[0] == 'Valve'
+      name = parts[1]
 
-    node = Node(name=name, nbor_names=nbor_names, rate=rate)
-    # print(node)
-    name2node[name] = node
+      ratestr = parts[4]
+      assert ratestr[-1] == ';'
+      rate = int(ratestr[len('rate='):-1])
+      
+      assert parts[8] in ['valves', 'valve']
+      tunnelstrs = parts[9:]
+      nbor_names = [s.replace(',', '') for s in tunnelstrs]
 
-# hook up nbors
-for node in name2node.values():
-  nbors = [name2node[name] for name in node.nbor_names]
-  node.nbors = nbors
+      node = Node(name=name, nbor_names=nbor_names, rate=rate)
+      # print(node)
+      name2node[name] = node
 
-find_max_release(name2node)
+  # hook up nbors
+  for node in name2node.values():
+    nbors = [name2node[name] for name in node.nbor_names]
+    node.nbors = nbors
+
+  find_max_release(name2node)
+
+main()
