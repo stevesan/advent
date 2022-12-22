995ee1bc2a29fbcc8c2ad5f77d97ac16864bdc61
Steven An <1388372+stevesan@users.noreply.github.com>
2022-12-22 08:45:44 -0500

    password calc done

--- d22.py
+++ d22.py
@@ -5,6 +5,14 @@ from util import Int2
 # right is first
 cw_dirs = [Int2(1, 0), Int2(0, -1), Int2(-1, 0), Int2(0, 1)]
 
+def get_pass(x, y, dir, height):
+  col = x + 1
+  row = height - y
+  print(row, col)
+  return 1000 * row + 4 * col + dir
+
+assert get_pass(7, 6, 0, 12) == 6032
+
 def solve(inputf):
   width = 0
   num_lines = 0
