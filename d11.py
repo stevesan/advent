b0bfd24855ffb1050a1e2a1c5f52370a73634f18
Steven An <1388372+stevesan@users.noreply.github.com>
2022-12-22 13:38:57 -0500

    start d11

---
+++ d11.py
@@ -0,0 +1,72 @@
+
+import sys, os
+from dataclasses import dataclass
+
+@dataclass
+class Monkey:
+  items:list[int]
+  op:str
+  oparg:int
+  testdivisor:int
+  true_throw_to:int
+  false_throw_to:int
+
+  def test(self, val):
+    return val % self.testdivisor == 0
+
+  def apply_op(self, old):
+    argval = old if self.oparg is None else self.oparg
+    if self.op == '*':
+      return old * argval
+    elif self.op == '+':
+      return old + argval
+    raise Exception(f'unknown op: {self.op}')
+
+def main(inputf):
+  with open(inputf, 'r') as f:
+    lines = f.readlines()
+
+  monkeys = []
+  i = 0
+  while 7*i < len(lines):
+    chunk = lines[7*i : 7*i + 7]
+    assert chunk[0].startswith('Monkey ')
+    itemsline = chunk[1]
+    opline = chunk[2]
+    testline = chunk[3]
+    trueline = chunk[4]
+    falseline = chunk[5]
+
+    items = [int(s.strip()) for s in itemsline.split(':')[1].split(',')]
+
+    op, argstr = opline.split(' old ')[1].split(' ')
+    assert op in ['+', '*']
+    if argstr == 'old\n':
+      argval = None
+    else: 
+      argval = int(argstr)
+
+    assert 'Test: divisible by ' in testline
+    testdivisor = int(testline.split(' by ')[1])
+
+    assert 'If true: throw to monkey ' in trueline
+    true_throw_to = int(trueline.split(' ')[-1])
+
+    assert 'If false: throw to monkey ' in falseline
+    false_throw_to = int(falseline.split(' ')[-1])
+
+    mon = Monkey(items=items, op=op, oparg=argval, testdivisor=testdivisor,
+      true_throw_to=true_throw_to,
+      false_throw_to=false_throw_to)
+
+    monkeys.append(mon)
+    print(mon)
+
+    i += 1
+  
+  return 1
+
+assert main('d11sample.txt')
+assert main('d11full.txt')
+
+# main(sys.argv[1])
