445a6da562fc479c65ef7dfb063fc8adecefde73
Steven An <1388372+stevesan@users.noreply.github.com>
2022-12-22 08:15:54 -0500

    speeding up d15a..

--- d15.py
+++ d15.py
@@ -12,6 +12,7 @@ class Sensor:
 
   def get_intxs(self, y):
     dy = self.pos.y - y
+    if dy > self.radius: return None
     dx = self.radius - abs(dy)
     return (self.pos.x - dx, self.pos.x + dx)
 
@@ -38,37 +39,56 @@ def main(inputf, query_y):
   all_xs = [s.pos.x for s in sensors] + [s.closest_beacon_pos.x for s in sensors]
   all_ys = [s.pos.y for s in sensors] + [s.closest_beacon_pos.y for s in sensors]
 
-  all_rads = [s.radius for s in sensors]
-  maxrad = max(all_rads)
-
   mins = Int2(min(all_xs), min(all_ys))
   maxs = Int2(max(all_xs), max(all_ys))
 
   print(mins, maxs)
 
-  excluded_spots = 0
+  pairs = []
+  for sen in sensors:
+    intxs = sen.get_intxs(query_y)
+    if intxs:
+      pairs.append([sen, intxs[0]])
+      pairs.append([sen, intxs[1]])
 
-  t0 = time.time()
-  # TODO actually we need to account for the largest radii as well..subtract that off
-  x0 = mins.x-maxrad
-  x1 = maxs.x + maxrad + 1
-  print('xrange', x0, x1)
-  for x in range(x0, x1):
-    t1 = time.time()
-    if t1 - t0 > 1:
-      t0 = t1
-      print(f'{x-mins.x+maxrad} of {maxs.x+maxrad+1-mins.x+maxrad}')
-    q = Int2(x, query_y)
-    if q in beacons or q in sensor_poss: continue
-    excluded = False
-    for sen in sensors:
-      qdist = q.mandist(sen.pos)
-      if qdist <= sen.radius:
-        excluded = True
-        break
-    if excluded:
-      excluded_spots += 1
-  print(excluded_spots)
+  pairs.sort(key=lambda p: p[1])
+  print(pairs)
+
+  count = 0
+  prev_x = pairs[0][1]
+  overlaps = [pairs[0][0]]
+  for pair in pairs[1:]:
+    sen = pair[0]
+    x = pair[1]
+    if len(overlaps) > 0:
+      count += x - prev_x
+
+    if len(overlaps) == 0 and prev_x < x:
+      count += 1
+
+    if sen in overlaps:
+      overlaps.remove(sen)
+    else:
+      overlaps.append(sen)
+    prev_x = x
+
+  # for end of last one
+  count += 1
+
+  # remove all actual beacon and sensor positions
+  unique_existings_at_y = set()
+  for sen in sensors:
+    if sen.closest_beacon_pos.y == query_y:
+      unique_existings_at_y.add(sen.closest_beacon_pos)
+    if sen.pos.y == query_y:
+      unique_existings_at_y.add(sen.pos)
+  count -= len(unique_existings_at_y)
+  print(f'final answer: {count}')
+  return count
+
+assert main('d15sample.txt', 10) == 26
+assert main('d15test1.txt', 10) == 1
+assert main('d15test2.txt', 10) == 0
+assert main('d15test3.txt', 10) == 0
 import sys
-
 main(sys.argv[1], int(sys.argv[2]))
