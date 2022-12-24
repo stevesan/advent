8315a38829ee177455442a04a4cfbfc1f9e80ab5
Steven An <1388372+stevesan@users.noreply.github.com>
2022-12-24 10:18:24 -0500

    heap version..definitely did not need this. just BFS would've been fine

--- d24.py
+++ d24.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from util import Int2
+import heapq
 
 DIR2DELTA = {
   '>':Int2(1, 0),
@@ -24,6 +25,42 @@ def print_frame(f, width, height):
 
 def make_frame():
   return defaultdict(lambda:[])
+
+
+def shortest_path(frames, width, height):
+  nframes = len(frames)
+  visited:set[(int, Int2)] = set()
+  p0 = Int2(0, height-1)
+  assert len(frames[0][p0]) == 0
+
+  endpt = Int2(width-1, 0)
+  Q:list[(int, Int2)] = []
+  pair2dist = {}
+  first_pair = (0, p0)
+  pair2dist[first_pair] = 0
+  heapq.heappush(Q, first_pair)
+
+  while Q:
+    time, p = heapq.heappop(Q)
+    if p == endpt:
+      print(f'reached end pt. time = {time}')
+      return time
+    if (p, time) in visited: continue
+    visited.add((p, time))
+    next_frame = frames[(time + 1) % nframes]
+    qdist = pair2dist[(p, time)] + 1
+    for dir in DIR2DELTA.values():
+      q = p + dir
+      nbor_pair = (q, time + 1)
+      if len(next_frame[q]) > 0:
+        # blocked
+        continue
+      if nbor_pair in visited: continue
+      if nbor_pair not in pair2dist or pair2dist[nbor_pair] > qdist:
+        pair2dist[nbor_pair] = qdist
+        # not updating - just pushing the new entry. we always check if
+        # the top item is already visited, so we won't have duplicates
+        heapq.heappush(Q, (qdist, nbor_pair))
 
 def step_blizzards(f, width, height):
   rv = make_frame()
@@ -65,8 +102,7 @@ def solve(inputf, period):
     f = step_blizzards(f, width, height)
     frames.append(f)
 
-  p0 = Int2(0, height-1)
-  assert len(frames[1][p0]) == 0
+  return shortest_path(frames, width, height)
 
 solve('d24sample.txt', 5)
 solve('d24s2.txt', 12)
