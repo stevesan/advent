e04ba16bbace9b1bd483b5538bab9b0a2f76ec8b
Steven An <1388372+stevesan@users.noreply.github.com>
2023-01-04 17:32:14 -0500

    box check

--- 2021/d19.py
+++ 2021/d19.py
@@ -2,21 +2,53 @@
 import sys
 import numpy as np
 
-with open(sys.argv[1]) as f:
-  text = f.read()
+R = 1000
 
-scanner2beacons = []
+def Pt(x, y, z): return np.array([x, y, z])
 
-for grouptext in text.split('\n\n'):
-  group = grouptext.split('\n')
-  header = group[0]
-  assert 'scanner' in header
-  beacons = []
-  for line in group[1:]:
-    p = np.array([int(x) for x in line.split(',')])
-    beacons.append(p)
-  scanner2beacons.append(beacons)
+def augment_coordinates(xx):
+  more = [xx - 2*R for x in xx]
+  return xx + more
 
-for bs in scanner2beacons:
-  print('---')
-  print(bs)
+def get_box_min(boxcenter):
+  return boxcenter - Pt(R, R, R)
+
+def in_box(boxmin, p):
+  center = boxmin + Pt(R, R, R)
+  delta = p - center
+  return all(abs(delta) <= R)
+
+assert in_box(get_box_min(Pt(500, 0, -500)), Pt(-500, 1000, -1500))
+assert in_box(get_box_min(Pt(500, 0, -500)), Pt(1500, -1000, 500))
+assert in_box(get_box_min(Pt(500, 0, -500)), Pt(1500, -1001, 500)) == False
+assert in_box(get_box_min(Pt(500, 0, -500)), Pt(1501, 0, -500)) == False
+
+def gen_overlaps(beacons:list[np.array]):
+  xs = augment_coordinates([p[0] for p in beacons])
+  ys = augment_coordinates([p[1] for p in beacons])
+  zs = augment_coordinates([p[2] for p in beacons])
+
+  for x in xs:
+    for y in ys:
+      for z in zs:
+        boxmin = np.array([x, y, z])
+        
+
+
+def main(inputf):
+  with open(inputf) as f:
+    text = f.read()
+
+  scanner2beacons = []
+
+  for grouptext in text.split('\n\n'):
+    group = grouptext.split('\n')
+    header = group[0]
+    assert 'scanner' in header
+    beacons = []
+    for line in group[1:]:
+      p = np.array([int(x) for x in line.split(',')])
+      beacons.append(p)
+    scanner2beacons.append(beacons)
+
+main(sys.argv[1])
